e2694fea5807f6e6ea063076ad353d0988d34ee5
RachelLindquist <firefly7491@gmail.com>
2022-06-05 13:45:20 -0400

    Completed Yearly Maintenance Cost

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Fake inputs
+        double springCost = 562.65;
+        double summerCost = 789.45;
+        double fallCost = 356.83;
+        double winterCost = 945.73;
+
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("With the cost of spring: " + springCost + " and the cost of summer: " + summerCost
+            + " and the cost of fall: " + fallCost + " and the cost of winter: " + winterCost +
+                ", the total yearly maintenance cost is " + totalCost);
+
     }
 }
